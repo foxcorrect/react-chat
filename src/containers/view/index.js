8f4317c7c74e3b7e390ab38c9415be2eaac4dfc9
GYW <1972065889@qq.com>
2020-03-17 16:31:00 +0800

    overview

--- src/containers/view/index.js
+++ src/containers/view/index.js
@@ -1,16 +1,28 @@
 import * as React from 'react';
 import styles from './index.less';
+import '../../fonts/home/style.css';
+import '../../fonts/person/style.css';
 
 export default class Layout extends React.Component {
     render() {
         return (
             <div>
-                <div>
+                <div className={styles.container}>
                     {this.props.children}
                 </div>
-                <div className={styles.footer}>
-                    <div></div>
-                    <div></div>
+                <div className={`${styles.display_com} ${styles.flex_direction_row} ${styles.justify_content_flex_center} ${styles.footer}`}>
+                    <div className={`${styles.display_com} ${styles.flex_direction_row} ${styles.justify_content_flex_center} ${styles.align_items_center} ${styles.dispaly_width}`}>
+                        <div className={styles.div_algin + ' ' + styles.cursor_pointer}>
+                            <p><i className="icon-home" /></p>
+                            <label>首页</label>
+                        </div>
+                    </div>
+                    <div className={`${styles.display_com} ${styles.flex_direction_row} ${styles.justify_content_flex_center} ${styles.align_items_center} ${styles.dispaly_width}`}>
+                        <div className={styles.div_algin + ' ' + styles.cursor_pointer}>
+                            <p><i className="icon-person" /></p>
+                            <label>我的</label>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- src/containers/view/overvoew.js
+++ src/containers/view/overvoew.js
@@ -1,9 +1,57 @@
 import * as React from 'react';
+import styles from './index.less';
+import '../../fonts/chat/style.css';
 
 export default class Overview extends React.Component {
     render() {
         return (
-            <div>11111</div>
+            <div style={{ height: '100%' }}>
+                <div className={styles.over_top}>
+                    <p className={styles.over_top_title}>二进制女孩</p>
+                    <h1 className={styles.over_top_subtitle}>有1就有0</h1>
+                    <div className={styles.over_top_subtitle_desc}>
+                        <div className={styles.over_top_box}></div>
+                        <div className={styles.over_top_subtitle_desc_txt}>相亲才会赢</div>
+                        <div className={styles.over_top_box}></div>
+                    </div>
+
+                </div>
+                <div className={styles.over_container}>
+                    <div className={`${styles.display_com} ${styles.over_item}`}>
+                        <div className={`${styles.display_com} ${styles.flex_direction_row} ${styles.justify_content_flex_center} ${styles.align_items_center} ${styles.item_avatar}`}>
+                            <span className={`${styles.avatar} ${styles.bk_edit}`}><i className="icon-edit" /></span>
+                        </div>
+                        <div className={`${styles.display_com} ${styles.align_items_center} ${styles.item_content}`}>
+                            <div className={styles.item_content_txt}>
+                                <p className={styles.item_content_title}>活动报名</p>
+                                <p className={styles.item_content_subtitle}>点击报名一周CP活动，填写个人资料报名</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${styles.display_com} ${styles.over_item}`}>
+                        <div className={`${styles.display_com} ${styles.flex_direction_row} ${styles.justify_content_flex_center} ${styles.align_items_center} ${styles.item_avatar}`}>
+                            <span className={`${styles.avatar} ${styles.bk_date}`}><i className="icon-date" /></span>
+                        </div>
+                        <div className={`${styles.display_com} ${styles.align_items_center} ${styles.item_content}`}>
+                            <div className={styles.item_content_txt}>
+                                <p className={styles.item_content_title}>每日签到</p>
+                                <p className={styles.item_content_subtitle}>报名成功，每日与CP参与签到</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${styles.display_com} ${styles.over_item}`}>
+                        <div className={`${styles.display_com} ${styles.flex_direction_row} ${styles.justify_content_flex_center} ${styles.align_items_center} ${styles.item_avatar}`}>
+                            <span className={`${styles.avatar} ${styles.bk_chat}`}><i className="icon-chat" /></span>
+                        </div>
+                        <div className={`${styles.display_com} ${styles.align_items_center} ${styles.item_content}`}>
+                            <div className={styles.item_content_txt}>
+                                <p className={styles.item_content_title}>聊天界面</p>
+                                <p className={styles.item_content_subtitle}>报名成功，点击与CP发起会话</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
